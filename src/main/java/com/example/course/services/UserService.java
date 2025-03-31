0fb5b8ead32d2768ef59999282047c3d86a43345
Janderson Jean <jandersonjean@gmail.com>
2025-03-31 07:18:21 -0300

    User update

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +29,14 @@ public class UserService {
 
     public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User user) {
+        User entity = repository.getReferenceById(id);
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
+        return repository.save(entity);
     }
 
 }
